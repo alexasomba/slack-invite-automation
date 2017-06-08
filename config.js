a0501097ce9e0d7661bf580c1a14d051f6451809
Alexander Asomba <alexanderasomba@gmail.com>
2017-06-08 14:16:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
   //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
   //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'BKSvyY3ecBtBaWzDByVCCmqt',
+  slacktoken: process.env.SLACK_TOKEN || 'xoxp-194780897621-194685660242-194763910260-f3e4c22690d5f7e80227c248909608ee',
   // an optional security measure - if it is set, then that token will be required to get invited.
   inviteToken: process.env.INVITE_TOKEN || null,
 
