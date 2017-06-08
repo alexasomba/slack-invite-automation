ee713bad478ea26140279bd48f0bb019e2250b36
Alexander Asomba <alexanderasomba@gmail.com>
2017-06-08 19:23:20 +0100

    Create config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'Intern To Expert by StartupYou',
+  community: process.env.COMMUNITY_NAME || '',
   // your slack team url (ex: socketio.slack.com)
   slackUrl: process.env.SLACK_URL || 'interntoexpert.slack.com',
   // access token of slack
@@ -12,7 +12,7 @@ module.exports = {
   //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
   //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
   //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'xoxp-194780897621-194685660242-194763910260-f3e4c22690d5f7e80227c248909608ee',
+  slacktoken: process.env.SLACK_TOKEN || '',
   // an optional security measure - if it is set, then that token will be required to get invited.
   inviteToken: process.env.INVITE_TOKEN || null,
 
